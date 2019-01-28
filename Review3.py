bd8ca05fd017385112fe840280b5b8f855d8fd21
ChelseaW <c.winfrey93@yahoo.com>
2019-01-28 13:04:02 -0600

    Not

---
+++ Review3.py
@@ -0,0 +1,38 @@
+def main():
+    # problem1()
+    problem2()
+    # problem3()
+    # problem4()
+
+def problem1():
+
+    n = int(input("Enter a number:"))
+    return ("True")
+
+
+def into10(n, outside_mode):
+    while(True):
+        if n >= 10: outside_mode >=10
+            return(True)
+        elif n >= 1: outside_mode >=1
+            return(True)
+        elif n <= 10: outside_mode <=10
+
+
+def problem2():
+
+
+
+    userInput = input("Enter your books")
+    books = ""
+    while(userInput != "="):
+        userInput = input("Wrong!")
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
